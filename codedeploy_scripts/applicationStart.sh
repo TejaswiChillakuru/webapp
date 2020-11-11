9a8c790ff9a1e3d7a3de475ff380c0163ce756bf
TejaswiChillakuru <chillakuru.t@northeastern.edu>
2020-11-11 13:44:26 -0500

    cloudwatch

--- codedeploy_scripts/applicationStart.sh
+++ codedeploy_scripts/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
 cd /home/ubuntu/target
 sudo java -jar *.jar
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
